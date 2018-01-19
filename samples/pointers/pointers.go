07513191f5dd8e0db2baf803955c4df2be1034a9
Sattar Hummatli <settarxan@gmail.com>
2018-01-19 10:33:27 +0400

    work with pointers

---
+++ samples/pointers/pointers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var p *int
+	if p != nil {
+		fmt.Println("Value of p:", *p)
+	} else {
+		fmt.Println("p is nil")
+	}
+
+	var v int = 42
+	p = &v
+
+	if p != nil {
+		fmt.Println("Value of p:", *p)
+	} else {
+		fmt.Println("p is nil")
+	}
+
+	var value1 float64 = 42.13
+	pointer1 := &value1
+	fmt.Println("value1: ", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("value1: ", *pointer1)
+	fmt.Println("value1: ", value1)
+
+}
