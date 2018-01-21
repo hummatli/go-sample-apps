d37db3a429f1092747df3568840d497305c51a02
Sattar Hummatli <settarxan@gmail.com>
2018-01-21 09:03:53 +0400

    add error samples

---
+++ samples/errors/errors.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("filename.txt")
+
+	if err == nil {
+		fmt.Println(f)
+	} else {
+		fmt.Println(err)
+	}
+
+	myError := errors.New("My error string")
+	fmt.Println(myError)
+
+	attendance := map[string]bool{
+		"Ann":  true,
+		"Mike": true}
+
+	attended, ok := attendance["Mike"]
+	if ok {
+		fmt.Println("Mike attended?", attended)
+	} else {
+		fmt.Println("No info for Mike")
+	}
+
+}
